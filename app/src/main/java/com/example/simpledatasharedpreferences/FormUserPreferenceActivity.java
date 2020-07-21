023668b7bdac1182f99d672c15de406d6cfb6cec
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-07-21 15:31:17 +0700

    add layout to FormUserPreferenceActivity

---
+++ app/src/main/java/com/example/simpledatasharedpreferences/FormUserPreferenceActivity.java
@@ -0,0 +1,14 @@
+package com.example.simpledatasharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormUserPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_user_preference);
+    }
+}
